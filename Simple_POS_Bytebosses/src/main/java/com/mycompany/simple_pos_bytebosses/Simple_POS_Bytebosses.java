4911002591b86cbf616e4801256acee87e56a9a1
anglrxi <pamilozanhessian@gmail.com>
2026-05-04 10:48:43 +0800

    Add files via upload

---
+++ Simple_POS_Bytebosses/src/main/java/com/mycompany/simple_pos_bytebosses/Simple_POS_Bytebosses.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.simple_pos_bytebosses;
+
+/**
+ *
+ * @author ABS_021823D
+ */
+public class Simple_POS_Bytebosses {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
